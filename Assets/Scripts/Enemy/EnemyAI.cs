fdee59dbdd94f6dc111c09232bb4dbd5c264af47
Bogglebo <diwillisnz@gmail.com>
2023-05-21 17:55:37 +1200

    Revamp enemy animations and navmesh
    Incorporate movement and animations via navmesh vs rigidbody

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -21,22 +21,65 @@ public class EnemyAI : MonoBehaviour
     // otherwise it would trigger on start as it would be zero
     float distanceToTarget = Mathf.Infinity;
 
+    // Get animator component
+    Animator animator;
+
+    public bool isProvoked = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Calculate the distance between the target and the enemy each frame
-        distanceToTarget = Vector3.Distance (target.position, agent.transform.position);
+        distanceToTarget = Vector3.Distance(target.position, agent.transform.position);
+
         // If the player is within the chase range set, pursue the player
         if (distanceToTarget < chaseRange)
         {
-            agent.SetDestination(target.position); // Move the enemy to the target's position
+            isProvoked = true;
+            EngageTarget();
         }
     }
+
+    // Method to decide whether the enemy should chase or attack the player
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= agent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= agent.stoppingDistance)
+            {
+                AttackTarget();
+        }
+    }
+
+    // Method to call when enemy is chasing the player
+    private void ChaseTarget()
+    {
+        animator.SetTrigger("run");
+        agent.SetDestination(target.position);  // Move the enemy to the target's position
+    }
+
+    // Method to call if enemy gets within attack range of the player
+    private void AttackTarget()
+    {
+        animator.SetBool("Attack", true);
+        Debug.Log("Enemy " + name + (" is attacking the  " + target.name ));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
 }
